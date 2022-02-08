c8e17e292af1132d33bd1f43e393bca9584bef3c
danieltistoj <jose.tisrey@gmail.com>
2022-02-08 13:36:05 -0600

    express routing y nodemon

--- express/servidor_express.js
+++ express/servidor_express.js
@@ -7,6 +7,20 @@ app.get('/',(req,res)=>{
     //como respuesta
     res.send("Hola mundo");
 });
+//si queremos crear otra ruta
+app.get('/about',(req,res)=>{
+    res.send('About me');
+})
+
+//si queremos crear otra ruta
+app.get('/contacto',(req,res)=>{
+    res.send('Fromulario de contacto');
+})
+
+app.get('/test',(req,res)=>{
+    res.send('<h1>TEST</h1>');
+})
+
 app.listen(3000,()=>{
     console.log("Servidor en el puerto 3000");
 });
