6a8b4cf83f81ee08026b6cb38658290085c0475f
danieltistoj <jose.tisrey@gmail.com>
2022-02-09 18:39:19 -0600

    middleware

--- express/servidor_express.js
+++ express/servidor_express.js
@@ -3,13 +3,29 @@ const express = require('express');
 
 //al ejecutar express nos devuelve un objeto que es mi servidor 
 const app = express();
+//morgan
+const morgan = require('morgan');
+//Middlewares
+//req: es la informacion del navegador
+function logger(req,res,next){
+    console.log(`Route Receives: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+
+}
+
 app.use(express.json());
+app.use(logger);
+app.use(morgan('dev'));
+
+
 //no es un metodo http. Es una funcion de express que para toda ruta se puede hacer algo
+/*
 app.all('/usuario',(req,res,next)=>{
     console.log('Por aqui paso');
     //res.send('Terminado');
     next();
 });
+*/
 //get es para devolver cosas
 app.get('/',(req,res)=>{
     //como respuesta
@@ -49,6 +65,7 @@ app.put('/usuario/:id',(req,res)=>{
 app.delete('/usuario/:usuarioId',(req,res)=>{
     res.send(`Usuario ${req.params.usuarioId} eliminado`);
 })
+app.use(express.static('./public/index.html'));
 
 app.listen(3000,()=>{
     console.log("Servidor en el puerto 3000");
